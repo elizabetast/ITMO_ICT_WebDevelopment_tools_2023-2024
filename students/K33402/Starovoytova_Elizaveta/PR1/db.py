8caca3cfeccdf7951130fdba25949a2d912b8167
Старовойтова Елизавета <starovojtovaelizaveta@MacBook-Air-Starovojtova.local>
2024-04-06 23:52:28 +0300

    add PR1

---
+++ students/K33402/Starovoytova_Elizaveta/PR1/db.py
@@ -0,0 +1,34 @@
+temp_bd = [
+    {
+        "id": 1,
+        "race": "director",
+        "name": "Старовойтова Лиза",
+        "level": 12,
+        "profession": {
+            "id": 2,
+            "title": "Старшая сестра",
+            "description": "Главная из сестер",
+        },
+    },
+    {
+        "id": 2,
+        "race": "worker",
+        "name": "Старовойтова Катя",
+        "level": 12,
+        "profession": {
+            "id": 1,
+            "title": "Младшая сестра",
+            "description": "Младше на 5 минут",
+        },
+    },
+]
+
+
+db_professions = [
+    {"id": 1, "title": "Старшая сестра", "description": "Главная из сестер"},
+    {
+        "id": 2,
+        "title": "Младшая сестра",
+        "description": "Младше на 5 минут",
+    },
+]
